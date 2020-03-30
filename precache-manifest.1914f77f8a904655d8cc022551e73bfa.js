103c61151adb8a46457240c57bdcd38522c600fb
AntonChetveryk <chetverykanton92@gmail.com>
2020-03-30 10:40:08 +0300

    Updates

---
+++ precache-manifest.1914f77f8a904655d8cc022551e73bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607149696d82ce464bbeac6b792095ce",
+    "url": "/AntonChetveryk/cursor-react/index.html"
+  },
+  {
+    "revision": "66c35de257265614c27c",
+    "url": "/AntonChetveryk/cursor-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c58e1f6767219fea5168",
+    "url": "/AntonChetveryk/cursor-react/static/css/main.a7bae26d.chunk.css"
+  },
+  {
+    "revision": "66c35de257265614c27c",
+    "url": "/AntonChetveryk/cursor-react/static/js/2.f23c83be.chunk.js"
+  },
+  {
+    "revision": "c58e1f6767219fea5168",
+    "url": "/AntonChetveryk/cursor-react/static/js/main.9766077d.chunk.js"
+  },
+  {
+    "revision": "6e702614009c5ba9bdc4",
+    "url": "/AntonChetveryk/cursor-react/static/js/runtime~main.91e42509.js"
+  }
+]);
